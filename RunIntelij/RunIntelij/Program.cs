3f921d15f6d010100544838b3f6af62efa5f1ea1
vanminh <vanminh020698@gmail.com>
2017-12-07 17:52:49 +0700

    update

---
+++ RunIntelij/RunIntelij/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RunIntelij
+{
+    class Program
+    {
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
+        static void Main(string[] args)
+        {
+            Process p = Process.Start(@"C:\Program Files\JetBrains\IntelliJ IDEA 2017.3\bin\idea64.exe");
+            p.WaitForInputIdle();
+        }
+    }
+}
